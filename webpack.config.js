8d360cb4f3778432648741a0109f4ffbf3ce2a1f
Svetlana Sunagatullina <Ssunagatullina@mail.ru>
2023-08-29 17:42:24 +0300

    create page, add fonts

---
+++ webpack.config.js
@@ -0,0 +1,122 @@
+const path = require('path'); 
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin'); 
+
+module.exports = {
+  entry: { 
+    main: './src/pages/index.js' 
+  },
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'main.js',
+    publicPath: ''
+  },
+  mode: 'development',
+  devServer: {
+    static: path.resolve(__dirname, './dist'), 
+    compress: true, 
+    port: 8080, 
+    open: true 
+  },
+  module: {
+    rules: [{
+        test: /\.js$/,
+        use: 'babel-loader',
+        exclude: '/node_modules/'
+      },
+      {
+        test: /\.(png|svg|jpg|gif|woff(2)?|eot|ttf|otf)$/,
+        type: 'asset/resource'
+      },
+      {
+        test: /\.css$/,
+        use: [MiniCssExtractPlugin.loader, {
+          loader: 'css-loader',
+          options: { 
+            importLoaders: 1 
+          }
+        },
+        'postcss-loader'
+      ]
+      },
+      ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/index.html'
+    }),
+    new CleanWebpackPlugin(),
+    new MiniCssExtractPlugin(),
+  ],
+} 
+
+/* const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+const pages = ['index'];
+
+
+module.exports = {
+  entry: pages.reduce((config, page) => {
+    config[page] = `./src/pages/${page}.js`;
+    return config;
+  }, {}),
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: '[name].js',
+    publicPath: '',
+  },
+  optimization: {
+    splitChunks: {
+      chunks: "all",
+    },
+  },
+  mode: 'development',
+  devServer: {
+    static: path.resolve(__dirname, './dist'),
+    open: true,
+    compress: true,
+    port: 8080
+  },
+  module: {
+    rules: [{
+        test: /\.js$/,
+        use: 'babel-loader',
+        exclude: '/node_modules/'
+      },
+      {
+        test: /\.(png|svg|jpg|gif|woff(2)?|eot|ttf|otf)$/,
+        type: 'asset/resource',
+      },
+      {
+        test: /\.css$/,
+        use: [MiniCssExtractPlugin.loader, {
+            loader: 'css-loader',
+            options: {
+              importLoaders: 1
+            }
+          },
+          'postcss-loader'
+        ]
+      },
+    ]
+  },
+  plugins: [].concat(
+    pages.map(
+      (page) =>
+        new HtmlWebpackPlugin({
+          inject: true,
+          template: `./src/${page}.html`,
+          filename: `${page}.html`,
+          chunks: [page],
+        })
+    ),
+    new CleanWebpackPlugin(),
+    new MiniCssExtractPlugin(),
+  ),
+}; */
+
+
