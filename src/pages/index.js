59c08cdc89b61e33350416b71bc5c49d52bfbe33
Svetlana Sunagatullina <Ssunagatullina@mail.ru>
2023-09-01 17:32:22 +0300

    feat: header, footer, burger menu, theme switcher

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,1 +1,13 @@
 import '../pages/index.css';
+
+/* burger-menu */
+const menu = document.querySelector('.menu');
+const burgerBtn = document.querySelector('.header__burger-btn');
+const burgerDashes = document.querySelectorAll('.header__burger-dash');
+
+burgerBtn.addEventListener('click', () => {
+  menu.classList.toggle('menu_open');
+  burgerDashes.forEach((burgerDash) => {
+    burgerDash.classList.toggle('header__burger-dash-crossed');
+  })
+})
