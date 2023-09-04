a2417425ff74193b8d5a3136df0a53a9ccb70b27
Svetlana Sunagatullina <Ssunagatullina@mail.ru>
2023-09-04 16:15:01 +0300

    feat: slider

--- src/pages/index.js
+++ src/pages/index.js
@@ -11,3 +11,25 @@ burgerBtn.addEventListener('click', () => {
     burgerDash.classList.toggle('header__burger-dash-crossed');
   })
 })
+
+/* slider */
+let offset = 0; //смещение от левого края
+const sliderList = document.querySelector('.slider__list');
+const sliderPrevBtn = document.querySelector('.slider__btn_type_prev');
+const sliderNextBtn = document.querySelector('.slider__btn_type_next');
+
+sliderNextBtn.addEventListener('click', () => {
+  offset = offset + 690;
+  if (offset > 1380) {
+    offset = 0;
+  }
+  sliderList.style.left = -offset + 'px';
+});
+
+sliderPrevBtn.addEventListener('click', () => {
+  offset = offset - 690;
+  if (offset < 0) {
+    offset = 1380;
+  }
+  sliderList.style.left = -offset + 'px';
+});
